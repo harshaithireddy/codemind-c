a10b058068a78cd555ee2fd9707aafa445eb02ff
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-01-06 20:18:49 +0530

    #include<stdio.h> #include<string.h>
    int main()
    {
      char str[100];
      int start,end;
      scanf("%[^
    ]c", str);
      scanf("%d", &start);
      scanf("%d", &end);
    
      for(int i=start; i<=end; i++)
      {
          printf("%c",str[i]);
      }
    }

---
+++ _Exact_Slice_Of_String_.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+  char str[100];
+  int start,end;
+  scanf("%[^
+]c", str);
+  scanf("%d", &start);
+  scanf("%d", &end);
+
+  for(int i=start; i<=end; i++)
+  {
+      printf("%c",str[i]);
+  }
+}
