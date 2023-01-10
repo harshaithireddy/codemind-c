3fb639290ead54ca4ca9f12252669e1f9f869e6b
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-01-10 19:43:51 +0530

    #include <stdio.h> int main() {     int n,i,j;     scanf("%d",&n);
        int a[n];
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
    
        for(i=0;i<n;i++)
        {
            int flag=0;
            for(j=0;j<i;j++)
            {
                if(a[i]==a[j])
                {
                    flag=1;
                    break;
                }
            }
            if(flag==0)
            {
                printf("%d ",a[i]);
            }
        }
    }

---
+++ unique_elements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        int flag=0;
+        for(j=0;j<i;j++)
+        {
+            if(a[i]==a[j])
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
