0a6fa9a416d4e4ece2a592390f837bef51d6a274
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-01-12 12:39:05 +0530

    #include <stdio.h>
    int main()
    {
        int n, sum=0, i;
        scanf("%d", &n);
        if (n>=1 && n<=30)
        {
            int arr[n];
            for (i=0; i<n; i++)
            {
                scanf("%d", &arr[i]);
            }
            for (i=0; i<n; i++)
            {
                sum=sum+arr[i];
            }
            printf("%d",sum);
        }
        else
        {
            printf("Enter a valid number");
        }
    }

--- Find_the_sum_of_elements_in_the_array_without_using_loops.c
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-    int n,sum=0,i;
-    scanf("%d",&n);
-    if(n>=1 && n<=30)
+    int n, sum=0, i;
+    scanf("%d", &n);
+    if (n>=1 && n<=30)
     {
         int arr[n];
-        for(i=0;i<n;i++)
+        for (i=0; i<n; i++)
         {
-            scanf("%d",&arr[i]);
+            scanf("%d", &arr[i]);
         }
-        for(i=0;i<n;i++)
+        for (i=0; i<n; i++)
         {
             sum=sum+arr[i];
         }
-        printf("%d", sum);
+        printf("%d",sum);
     }
     else
     {
