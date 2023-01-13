0263522c1c091d7c8dc8ae0c032cc1f6d01bef8a
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-01-13 08:30:15 +0530

    #include <stdio.h>
    int main() {
        int hurl, spin, speed, grade;
        scanf("%d %d %d", &hurl, &spin, &speed);
    
        if (hurl > 50 && spin > 60 && speed > 100) {
            grade = 10;
        } else if (hurl > 50 && spin > 60) {
            grade = 9;
        } else if (spin > 60 && speed > 100) {
            grade = 8;
        } else if (hurl > 50 && speed > 100) {
            grade = 7;
        } else if (hurl > 50 || spin > 60 || speed > 100) {
            grade = 6;
        } else {
            grade = 5;
        }
        printf("%d", grade);
    }

--- Grades_of_Rides.c
+++ Grades_of_Rides.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
 
-int main() 
-{
+int main() {
     int hurl, spin, speed, grade;
     scanf("%d %d %d", &hurl, &spin, &speed);
 
-    if (hurl > 50 && spin > 60 && speed > 100) 
-    {
+    if (hurl > 50 && spin > 60 && speed > 100) {
         grade = 10;
-    } 
-    else if (hurl > 50 && spin > 60)
-    {
+    } else if (hurl > 50 && spin > 60) {
         grade = 9;
-    } 
-    else if (spin > 60 && speed > 100) 
-    {
+    } else if (spin > 60 && speed > 100) {
         grade = 8;
-    } 
-    else if (hurl > 50 && speed > 100) 
-    {
+    } else if (hurl > 50 && speed > 100) {
         grade = 7;
-    } 
-    else if (hurl > 50 || spin > 60 || speed > 100)
-    {
+    } else if (hurl > 50 || spin > 60 || speed > 100) {
         grade = 6;
-    } 
-    else
-    {
+    } else {
         grade = 5;
     }
     printf("%d", grade);
