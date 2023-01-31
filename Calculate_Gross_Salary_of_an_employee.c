63aef745fd4ff9d1ead5a8945cfd45b218e276ed
harshaithireddy <119839919+harshaithireddy@users.noreply.github.com>
2023-01-31 22:00:33 +0530

    #include<stdio.h> int main() {     float b,h,d,pf;     scanf("%f%f%f",&b,&h,&d);     pf=(b*12)/100;     float gr=b+h+d+pf;     printf("%0.2f %0.2f",pf,gr); }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    float b,h,d,pf,gr;
+    float b,h,d,pf;
     scanf("%f%f%f",&b,&h,&d);
-    pf=b*12/100;
-    gr=b+h+d+pf;
+    pf=(b*12)/100;
+    float gr=b+h+d+pf;
     printf("%0.2f
 %0.2f",pf,gr);
 }
